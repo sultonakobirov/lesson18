9ccb0c1a53769119c89baa83846656d3b219fd8a
sultonakobirov <zafarzhanovich@gmail.com>
2023-04-18 10:31:24 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// let url = "https://images.unsplash.com/photo-1681470137474-c32e29517885?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80"
+// let image = document.querySelector('img')
+// image.setAttribute('src', url)
+// image.alt = 'Гуччи флип флопс'
+// console.log(image.getAttribute('src'));
