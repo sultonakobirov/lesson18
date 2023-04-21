ddcc0634c641e947e9d73a2dbed09d145f96adfb
sultonakobirov <zafarzhanovich@gmail.com>
2023-04-21 12:50:24 +0500

    "optimizing"

--- js/script.js
+++ js/script.js
@@ -5,10 +5,16 @@ let image = doc.querySelector('.mbp-image')
 let color = doc.querySelector('.color')
 let imageOne = './images/white.jpg'
 let imageTwo = './images/spacegrey.jpg'
+let price = doc.querySelector('.price')
 
 let background = (btnOne, btnTwo) =>{
     btnOne.style.backgroundColor = '#0071E3'
     btnTwo.style.backgroundColor = '#CFE7FF'
+}
+if(color.textContent === "White"){
+    buttonWhite.style.backgroundColor = '#0071E3'
+}else{
+    buttonSpaceGrey.style.backgroundColor = '#CFE7FF'
 }
 
 buttonWhite.onclick = () =>{
@@ -21,58 +27,27 @@ buttonSpaceGrey.onclick = () =>{
     color.textContent = "Space Gray"
     background(buttonSpaceGrey, buttonWhite)
 }
-
-let storageOne = doc.querySelector('.storage-one')
-let storageTwo = doc.querySelector('.storage-two')
-let storageThree = doc.querySelector('.storage-three')
-let storageFour = doc.querySelector('.storage-four')
-let price = doc.querySelector('.price')
-let innicialPrice = 1999
-let priceOne = 200
-let priceTwo = 600
-let priceThree = 1200
-
-let borderColor = (btnOne, btnTwo) =>{
-    btnOne.style.borderColor = '#0071E3'
-    btnTwo.style.borderColor = '#CFE7FF'
-}
-
-storageOne.onclick = () =>{
-    borderColor(storageOne, storageFour)
-    borderColor(storageOne, storageThree)
-    borderColor(storageOne, storageTwo)
-    let firstPart =`$${innicialPrice}`.toString().slice(0, 2)
-    let secondPart = `$${innicialPrice}`.toString().slice(2, 7)
-    secondPart = firstPart+','+secondPart
-    price.textContent = secondPart
-
-    
-}
-storageTwo.onclick = () =>{
-    borderColor(storageTwo, storageOne)
-    borderColor(storageTwo, storageThree)
-    borderColor(storageTwo, storageFour)
-    let firstPart =`$${innicialPrice + priceOne}`.toString().slice(0, 2)
-    let secondPart = `$${innicialPrice + priceOne}`.toString().slice(2, 7)
-    secondPart = firstPart+','+secondPart
-    price.textContent = secondPart
-}
-storageThree.onclick = () =>{
-    borderColor(storageThree, storageTwo)
-    borderColor(storageThree, storageOne)
-    borderColor(storageThree, storageFour)
-    let firstPart =`$${innicialPrice + priceTwo}`.toString().slice(0, 2)
-    let secondPart = `$${innicialPrice + priceTwo}`.toString().slice(2, 7)
-    secondPart = firstPart+','+secondPart
-    price.textContent = secondPart
-}
-storageFour.onclick = () =>{
-    borderColor(storageFour, storageThree)
-    borderColor(storageFour, storageOne)
-    borderColor(storageFour, storageTwo)
-    let firstPart =`$${innicialPrice + priceThree}`.toString().slice(0, 2)
-    let secondPart = `$${innicialPrice + priceThree}`.toString().slice(2, 7)
-    secondPart = firstPart+','+secondPart
-    price.textContent = secondPart
-}
+let storageButtonsCont = doc.querySelectorAll('.button')
+storageButtonsCont[0].classList.add('active')
+storageButtonsCont.forEach(item => {
+    // Добавляем обработчик события 'click'
+    item.addEventListener('click', () => {
+    // Удаляем класс 'active' у всех кнопок
+    storageButtonsCont.forEach(item => {
+    item.classList.remove('active');
+    });
+    // Добавляем класс 'active' текущей выбранной кнопке
+    item.classList.add('active');
+    let getValue = item.querySelector('.active')
+    let  value = item.value
+    if(value == 0){
+        price.textContent = '$1,999'
+    }else if (value == 200) {
+        price.textContent = '$2,199'
+    } else if(value == 600){
+        price.textContent = '$2,599'
+    } else if(value == 1200)
+        price.textContent = '$3,199'
+    });
+    });
 
